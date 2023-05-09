5f9fba0b8d04005f3bd864c539c9cd502942df6b
DIR\alba.lima.garcia <albalimav70@hotmail.com>
2023-05-09 21:10:16 +0200

    Actualización AllergenDao

--- src/main/java/com/fpdual/persistence/aplication/dao/AllergenDao.java
+++ src/main/java/com/fpdual/persistence/aplication/dao/AllergenDao.java
@@ -1,6 +1,32 @@
 package com.fpdual.persistence.aplication.dao;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class AllergenDao {
     private int id;
     private String name;
+    private List<IngredientDao> ingredients;
+
+    public AllergenDao(ResultSet result) {
+        try {
+            this.id = result.getInt("id");
+            this.name = result.getString("name");
+            this.ingredients = new ArrayList<>();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
