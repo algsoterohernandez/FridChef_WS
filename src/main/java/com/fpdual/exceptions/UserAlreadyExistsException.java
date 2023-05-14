357dec1f637fccfff686d470a34ffe13c15ce090
Marta <mromper607j@g.educaand.es>
2023-05-14 18:28:10 +0200

    Tests realizados:  UserServiceTest  UserDaoTest  UserServiceTest

--- src/main/java/com/fpdual/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.fpdual.exceptions;
-
-public class UserAlreadyExistsException extends Exception{
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
