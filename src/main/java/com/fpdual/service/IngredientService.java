f190d370dcce25464da81b6bcee19a7033541340
Marta <mromper607j@g.educaand.es>
2023-05-27 21:08:04 +0200

    delete y create de ingredients

--- src/main/java/com/fpdual/service/IngredientService.java
+++ src/main/java/com/fpdual/service/IngredientService.java
@@ -1,8 +1,12 @@
 package com.fpdual.service;
 
 import com.fpdual.api.dto.IngredientDto;
+import com.fpdual.api.dto.UserDto;
+import com.fpdual.exceptions.FridChefException;
+import com.fpdual.exceptions.UserAlreadyExistsException;
 import com.fpdual.persistence.aplication.connector.MySQLConnector;
 import com.fpdual.persistence.aplication.dao.IngredientDao;
+import com.fpdual.persistence.aplication.dao.UserDao;
 import com.fpdual.persistence.aplication.manager.IngredientManager;
 import com.fpdual.utils.MappingUtils;
 
@@ -36,4 +40,44 @@ public class IngredientService {
 
         return ingredientDtos;
     }
+
+    public boolean deleteIngredient(int id) throws SQLException, ClassNotFoundException {
+        boolean deleted;
+
+        try (Connection con = connector.getMySQLConnection()) {
+            //Borramos el ingrediente
+            deleted = this.ingredientManager.deleteIngredient(con, id);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+
+        return deleted;
+    }
+
+    public IngredientDto createIngredient(String name) throws SQLException, ClassNotFoundException, FridChefException {
+        IngredientDto ingredientDto = null;
+
+        List<IngredientDto> ingredients = findAll();
+
+        //Si no hay u ingrediente con el mismo nombre, lo creamos
+        if(!ingredients.stream().anyMatch(o -> o.getName().equals(name))){
+
+            try (Connection con = connector.getMySQLConnection()) {
+
+                IngredientDao ingredientDao = this.ingredientManager.insertIngredient(con, name);
+                if (ingredientDao != null) {
+                    ingredientDto = MappingUtils.mapIngredientToDto(ingredientDao);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                throw e;
+            }
+        }
+        return ingredientDto;
+
+    }
+
+
 }
