44325dee2029eb79ebd64aa77e210cd489dfd0b2
Marta <mromper607j@g.educaand.es>
2023-05-29 09:08:10 +0200

    Gestión de la tabla ingredientes admin

--- src/main/java/com/fpdual/service/IngredientService.java
+++ src/main/java/com/fpdual/service/IngredientService.java
@@ -36,4 +36,44 @@ public class IngredientService {
 
         return ingredientDtos;
     }
+
+    public boolean deleteIngredient(int id) throws SQLException, ClassNotFoundException {
+        boolean deleted;
+
+        try (Connection con = connector.getMySQLConnection()) {
+            //Borramos el ingrediente
+            deleted = this.ingredientManager.deleteIngredient(con, id);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+
+        return deleted;
+    }
+
+    public IngredientDto createIngredient(String name) throws SQLException, ClassNotFoundException {
+        IngredientDto ingredientDto = null;
+
+        List<IngredientDto> ingredients = findAll();
+
+        //Si no hay un ingrediente con el mismo nombre, lo creamos
+        if(!ingredients.stream().anyMatch(o -> o.getName().equals(name))){
+
+            try (Connection con = connector.getMySQLConnection()) {
+
+                IngredientDao ingredientDao = this.ingredientManager.insertIngredient(con, name);
+                if (ingredientDao != null) {
+                    ingredientDto = MappingUtils.mapIngredientToDto(ingredientDao);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                throw e;
+            }
+        }
+        return ingredientDto;
+
+    }
+
+
 }
